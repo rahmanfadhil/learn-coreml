c594454203062269a2fb3f23137febc79db5ea4c
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-05-14 10:14:28 +0700

    Initial Commit

---
+++ LearnCoreML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnCoreML
+//
+//  Created by Rahman Fadhil on 14/05/20.
+//  Copyright © 2020 Rahman Fadhil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
